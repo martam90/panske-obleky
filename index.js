dce9633773b78c7076d0199328773c8db3b097cc
Marta Banas <banas.m.marta@gmail.com>
2021-04-26 19:40:57 +0200

    pridani tlacitek

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 'use strict';
 
 console.log('funguju');
+
+const whiteBtn = document.querySelector('.white');
+const blackBtn = document.querySelector('.black');
+const redBtn = document.querySelector('.red');
+const blueBtn = document.querySelector('.blue');
+
+const shirt = document.querySelector('#product-image');
+
+whiteBtn.addEventListener('click', () => {
+  shirt.style.fill = 'white';
+});
+
+blackBtn.addEventListener('click', () => {
+  shirt.style.fill = 'black';
+});
+
+redBtn.addEventListener('click', () => {
+  shirt.style.fill = 'red';
+});
+
+blueBtn.addEventListener('click', () => {
+  shirt.style.fill = 'lightblue';
+});
